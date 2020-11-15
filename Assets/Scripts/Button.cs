336f3a36e47c3b0e31db9cf3be499e1e37a313b2
Baptiste Rousserie <b.rousserie24@gmail.com>
2020-11-15 18:18:34 +0100

    Scripts

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -22,34 +22,3 @@ public class Button : ScriptableObject
     public IconType IconType;
     public ButtonPosition ButtonPosition;
 }
-
-[CreateAssetMenu]
-public class ButtonsManager : ScriptableObject
-{
-    public static ButtonsManager Instance;
-
-    private static Button lastButton;
-
-    public List<Button> buttons;
-    public Button firstButton;
-    
-    private void Awake()
-    {
-        if (Instance) Destroy(this);
-        else Instance = this;
-    }
-
-    public Button GetRandomButton(IconType iconType = IconType.Any)
-    {
-        List<Button> desiredButtons = buttons.Where(b => b != lastButton).ToList();
-        if (iconType != IconType.Any) 
-            desiredButtons = desiredButtons.Where(b => b.IconType == iconType).ToList();
-        
-        return lastButton = desiredButtons[Random.Range(0, desiredButtons.Count)];
-    }
-
-    public Button GetFirstButton()
-    {
-        return firstButton;
-    }
-}
---
+++ Assets/Scripts/ButtonDisplayersQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDisplayersQueue : MonoBehaviour
+{
+    private List<ButtonDisplayer> buttons;
+    private float buttonWidth;
+    private Camera cam;
+    
+    public void CreateButtonDisplayers(int size)
+    {
+        cam = Camera.main;
+        buttons = new List<ButtonDisplayer>();
+
+        float padding = Screen.width * 0.1f; 
+        float displayArea = Screen.width  - padding * 2;
+        
+        buttonWidth = (displayArea * 2) / (size * 3);
+        
+        for (int button = 0; button < size; button++)
+        {
+            GameObject newButtonDisplayer = new GameObject(
+                "ButtonDisplayer"+button, typeof(ButtonDisplayer));
+            newButtonDisplayer.transform.parent = transform;
+            newButtonDisplayer.transform.localPosition = cam.ScreenToWorldPoint(
+                new Vector3(padding + (float) (button + 0.5) * displayArea / size, cam.pixelHeight/2, 0));
+            newButtonDisplayer.transform.localPosition += new Vector3(0, 0, 1);
+            buttons.Add(newButtonDisplayer.GetComponent<ButtonDisplayer>());
+        }
+    }
+
+    public void SetSprite(int button, Sprite sprite)
+    {
+        buttons[button].SetSprite(sprite);
+        float spriteLargestBorder = Math.Max(sprite.rect.width, sprite.rect.height);
+        float scale = buttonWidth / spriteLargestBorder;
+        buttons[button].transform.localScale = new Vector3(scale, scale, 0);
+        buttons[button].gameObject.SetActive(true);
+    }
+
+    public void SetDone(int button)
+    {
+        buttons[button].gameObject.SetActive(false);
+    }
+}
